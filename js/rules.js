c7ebe445bf8f1f704a71e2c8a4d512cfd6966964
pkamath2 <git.pkamath2@gmail.com>
2018-08-10 23:59:44 +0800

    More rules added

--- js/rules.js
+++ js/rules.js
@@ -15,6 +15,40 @@ rule = (rule_num, num_squares) => {
             if(rule_num==1) fill = !(cell_state[i_prev] | cell_state[i_curr] | cell_state[i_next]);
             if(rule_num==2) fill = !(cell_state[i_prev]) & !(cell_state[i_curr]) & cell_state[i_next];
             if(rule_num==3) fill = !(cell_state[i_prev] | cell_state[i_curr]);
+            if(rule_num==4) fill = !(cell_state[i_prev] | cell_state[i_next]) & cell_state[i_curr];
+            if(rule_num==5) fill = !(cell_state[i_prev] | cell_state[i_next]);
+            if(rule_num==6) fill = !(cell_state[i_prev]) | (cell_state[i_curr] ^ cell_state[i_next]);
+            if(rule_num==7) fill = !(cell_state[i_prev] | cell_state[i_curr] & cell_state[i_next]);
+            if(rule_num==8) fill = !(cell_state[i_prev]) & cell_state[i_curr] & cell_state[i_next];
+            if(rule_num==9) fill = !(cell_state[i_prev] | (cell_state[i_curr] ^ cell_state[i_next]));
+            if(rule_num==10) fill = !(cell_state[i_prev]) & cell_state[i_next];
+            
+            if(rule_num==11) fill = cell_state[i_prev] ^ (cell_state[i_prev] | !cell_state[i_curr] | cell_state[i_next]);
+            if(rule_num==12) fill = (cell_state[i_prev] & cell_state[i_curr]) ^ cell_state[i_curr];
+            if(rule_num==13) fill = cell_state[i_prev] ^ (cell_state[i_prev] | cell_state[i_curr] | !cell_state[i_next]);
+            
+            if(rule_num==45) fill = cell_state[i_prev] ^ (cell_state[i_curr] | !cell_state[i_next]);
+            
+            if(rule_num==90) fill = cell_state[i_prev] ^ cell_state[i_next];
+
+            if(rule_num==105) fill = (cell_state[i_prev] ^ cell_state[i_curr] ^ !cell_state[i_next]);
+            if(rule_num==109) fill = cell_state[i_prev] ^ (cell_state[i_prev] | !cell_state[i_curr] | cell_state[i_next]) ^ cell_state[i_curr] ^ cell_state[i_next];
+            if(rule_num==110) fill = (!cell_state[i_prev] & cell_state[i_curr] & cell_state[i_next]) ^ cell_state[i_curr] ^ cell_state[i_next];
+
+            if(rule_num==119) fill = !(cell_state[i_curr] | cell_state[i_next]);
+
+            if(rule_num==121) fill = cell_state[i_prev] ^ (!(cell_state[i_prev]) | cell_state[i_curr] | cell_state[i_next]) ^ cell_state[i_curr] ^ cell_state[i_next];
+            if(rule_num==122) fill = cell_state[i_prev] ^ (cell_state[i_prev] & !cell_state[i_curr] & cell_state[i_next]) ^ cell_state[i_curr] ^ cell_state[i_next];
+
+            if(rule_num==137) fill = (!(cell_state[i_prev]) | cell_state[i_curr] | cell_state[i_next]) ^ cell_state[i_curr] ^ cell_state[i_next];
+
+            if(rule_num==182) fill = (cell_state[i_prev] & cell_state[i_next]) | (cell_state[i_prev] ^ cell_state[i_curr] ^ cell_state[i_next]);
+
+            if(rule_num==193) fill = cell_state[i_prev] ^ (cell_state[i_prev] | cell_state[i_curr] | !cell_state[i_next]) ^ cell_state[i_curr];
+
+
+            if(rule_num==225) fill = cell_state[i_prev] ^ !(cell_state[i_curr] | cell_state[i_next]);
+
             cell_state[i] = fill;
             fill = fill?"#123":"#fff"
         }
@@ -38,7 +72,7 @@ addrules = () => {
 createSpace = (rule_num) => {
     var space = new CanvasSpace("#ca-rule").setup({retina: true,bgcolor: 'floralwhite',resize:false});
     var form = space.getForm();
-    var num_squares = 100;
+    var num_squares = 300;
     var cells = [];
     var cell_state = [];
     space.add({
@@ -48,7 +82,7 @@ createSpace = (rule_num) => {
         },
         animate: (time, ftime) => {
             cells.forEach((c,i) => {
-                form.strokeOnly("#123", 0.5).fill(cell_state[i]?"#4e5054":"#fff").square(c, 3); 
+                form.strokeOnly("#123", 0.5).fill(cell_state[i]?"#4e5054":"#fff").square(c, 2); 
             });
         }
     });
